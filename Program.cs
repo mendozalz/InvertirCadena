f16b98fea5ac7a8fedd425bdd087a5d1987f28c9
Mendozalz <mendoza124302@gmail.com>
2024-02-08 20:20:46 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvertirCadena
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa calquier texto para invertir sus caracteres");
+            string cadenaIngresada = Console.ReadLine();
+
+            Console.WriteLine("El resultado de la cadena invertida es: ");
+            Console.WriteLine(InvertirManualmente(cadenaIngresada));
+            Console.Read();
+        }
+        public static string InvertirManualmente(string cadena)
+        {
+            string cadenaInvertida = "";
+            foreach (char letra in cadena)
+            {
+                cadenaInvertida = letra + cadenaInvertida;
+            }
+            return cadenaInvertida;
+        }
+    }
+}
